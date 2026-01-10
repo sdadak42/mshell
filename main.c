90328cbf67846c850676267fef92896b4727434b
Seyda Dadak <sdadak@k1m04s10.42istanbul.com.tr>
2026-01-10 20:27:18 +0300

    feat: token linked list added

--- main.c
+++ main.c
@@ -12,41 +12,62 @@
 
 #include "minishell.h"
 
-t_token	*ft_lexer_filter(char *line)
+void	ft_add_token(t_mdata *data, t_token *new_token)
 {
-	t_token	*token;
+	t_token	*temp;
 
-
+	if (!(data->tokens))
+		data->tokens = new_token;
+	else
+	{
+		temp = data->tokens;
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = new_token;
+		new_token->next = NULL;
+	}
 }
 
-void	ft_lexer(char *line)
+int	ft_token_word(t_mdata *data, char *line)
 {
 	t_token	*token;
 	int	i;
-	int	j;
 
-	token = malloc(sizeof(t_token));
 	i = 0;
-	j = 0;
+	token = malloc(sizeof(t_token));
+	if (ft_isalpha(line[i]))
+	{
+		while (ft_isalpha(line[i]))
+			i++;
+		token->value = ft_substr(line, 0, i);
+		token->type = WORD;
+		token->next = NULL;
+		ft_add_token(data, token);
+	}
+	return (i);
+}
+
+void	ft_lexer(t_mdata *data, char *line)
+{
+	int	i;
+
+	i = 0;
 	while (line[i])
 	{
 		if (ft_isalpha(line[i]))
-		{
-			j = i;
-			while (ft_isalpha(line[i + 1]))
-				i++;
-			token->value = ft_substr(line, j, i - j + 1);
-			token->type = WORD;
-		}
+			i += ft_token_word(data, &line[i]);
 		i++;
 	}
-	printf("value: %s\ntype: %d\n", token->value, token->type);
 }
 
 int	main(void)
 {
+	t_mdata	*data;
+	t_token	*temp;
+	t_token	*freele;
 	char	*line;
 
+	data = malloc(sizeof(t_mdata));
 	while(1)
 	{
 		line = readline("minishell$> ");
@@ -56,9 +77,22 @@ int	main(void)
 		{
 			add_history(line);
 		}
-		/* write(1, line, ft_strlen(line));
-		write(1, "\n", 1); */
-		ft_lexer(line);
+		ft_lexer(data, line);
+		temp = data->tokens;
+		while (temp)
+		{
+			write(1, temp->value, ft_strlen(temp->value));
+			write(1, "\n", 1);
+			temp = temp->next;
+		}
+		temp = data->tokens;
+		while (temp)
+		{
+			freele = temp;
+			temp = temp->next;
+			free(freele->value);
+			free(freele);
+		}
 		free(line);
 	}
 }
